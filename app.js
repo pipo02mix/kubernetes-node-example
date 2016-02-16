939a0b408469e75b6e9d563f84fd94d643dc4cf4
Fernando Ripoll iOS <info@tiatere.es>
2016-02-16 11:24:33 +0100

    Adding hostname to show how kubernetes balance work

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from ' + process.env.GET_HOSTS_FROM + '!');
+  res.send('Hello World from ' + process.env.HOSTNAME + '!');
 });
 
 app.listen(3000, function () {
